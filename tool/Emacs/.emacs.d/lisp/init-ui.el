565d253a14465a9ea556d903ecb1d9bda3a36754
comcx <comcx@outlook.com>
2019-02-04 14:14:37 +0800

    emacs

--- tool/Emacs/.emacs.d/lisp/init-ui.el
+++ tool/Emacs/.emacs.d/lisp/init-ui.el
@@ -79,7 +79,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 143 :width normal)))))
+ '(default ((t (:family "Source Code Pro" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
 
 
 ;; Set line number display

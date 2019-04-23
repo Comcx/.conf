f86e5a174ca361ffa10e706767e7253b88bd73dd
Comcx <comcx@outlook.com>
2019-04-23 15:31:40 +0800

    update

---
+++ program/C++/Mix.h
@@ -0,0 +1,45 @@
+/**
+ * #+TITLE:  Mix Library
+ * #+AUTHOR: Comcx
+ *
+ */
+
+
+#ifndef MIX_H
+#define MIX_H
+
+#define var    auto
+#define val    const auto
+#define mix    template
+#define Set    class
+#define use    using
+#define Type   typename
+#define module namespace
+
+namespace MIX {
+
+  using Int    = int;
+  using Unit   = void;
+  using Bool   = bool;
+  using Char   = char;
+  using Float  = float;
+  using Double = double;
+
+
+
+  
+
+
+}
+
+
+
+#endif
+
+
+
+
+
+
+
+

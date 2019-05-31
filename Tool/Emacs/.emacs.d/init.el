cd7db98f95c6e51624296a3eed786fd5d76c805d
Comcx <comcx@outlook.com>
2019-05-31 18:03:12 +0800

    sort

---
+++ Tool/Emacs/.emacs.d/init.el
@@ -0,0 +1,146 @@
+﻿
+;; Comcx's Emacs settings
+;; Make sure what you're doing when trying to change anything.
+;; 2018-12-20
+
+
+
+
+(package-initialize)
+(set-language-environment 'English)
+
+
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+(add-to-list 'load-path "~/.emacs.d/elpa/")
+(setq default-directory "~/workspace/") ; Change if you need
+
+
+;; Package Management
+;; -----------------------------------------------------------------
+(require 'init-packages)	; Package management
+(require 'init-ui)		; UI management
+(require 'init-keybindings)	; Key bindings
+(require 'init-org)		; Org mode settings
+;(require 'init-web)		; Web mode settings
+
+
+;;(require 'cmuscheme)
+
+
+;; Do not make backup files
+(setq make-backup-files nil)
+
+
+
+;; Custom settings
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+;; '(custom-enabled-themes (quote (wombat)))
+ '(package-selected-packages (quote (company)))
+ '(scroll-bar-mode nil)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil))
+
+
+(setq ring-bell-function 'ignore)
+
+
+
+
+;; ==================================================================================
+;; Some package settings
+
+
+;; Complete Any mode
+(global-company-mode 1)
+
+
+;; Neotree added
+(add-to-list 'load-path "~/.emacs.d/elpa/neotree")
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+(setq neo-theme 'ascii)
+
+
+
+(use-package ensime
+  :ensure t
+  :pin melpa)
+
+
+
+
+
+
+;;;;;;;;;;;;
+;; Scheme 
+;;;;;;;;;;;;
+
+(require 'cmuscheme)
+(setq scheme-program-name "racket")         ;; 如果用 Petite 就改成 "petite"
+
+
+;; bypass the interactive question and start the default interpreter
+(defun scheme-proc ()
+  "Return the current Scheme process, starting one if necessary."
+  (unless (and scheme-buffer
+               (get-buffer scheme-buffer)
+               (comint-check-proc scheme-buffer))
+    (save-window-excursion
+      (run-scheme scheme-program-name)))
+  (or (scheme-get-process)
+      (error "No current process. See variable `scheme-buffer'")))
+
+
+(defun scheme-split-window ()
+  (cond
+   ((= 1 (count-windows))
+    (delete-other-windows)
+    (split-window-vertically (floor (* 0.68 (window-height))))
+    (other-window 1)
+    (switch-to-buffer "*scheme*")
+    (other-window 1))
+   ((not (find "*scheme*"
+               (mapcar (lambda (w) (buffer-name (window-buffer w)))
+                       (window-list))
+               :test 'equal))
+    (other-window 1)
+    (switch-to-buffer "*scheme*")
+    (other-window -1))))
+
+
+(defun scheme-send-last-sexp-split-window ()
+  (interactive)
+  (scheme-split-window)
+  (scheme-send-last-sexp))
+
+
+(defun scheme-send-definition-split-window ()
+  (interactive)
+  (scheme-split-window)
+  (scheme-send-definition))
+
+(add-hook 'scheme-mode-hook
+  (lambda ()
+    (paredit-mode 1)
+    (define-key scheme-mode-map (kbd "<f5>") 'scheme-send-last-sexp-split-window)
+    (define-key scheme-mode-map (kbd "<f6>") 'scheme-send-definition-split-window)))
+
+
+
+
+
+
+
+
+
+
+

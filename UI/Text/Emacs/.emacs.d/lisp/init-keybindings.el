1ad88f7d4e794896ee45de7173c6257d30c3e14d
Comcx <comcx@outlook.com>
2019-08-12 01:59:21 +0800

    shell

--- UI/Text/Emacs/.emacs.d/lisp/init-keybindings.el
+++ UI/Text/Emacs/.emacs.d/lisp/init-keybindings.el
@@ -3,15 +3,23 @@
 
 
 ;; Shortcut for init.el
-(defun open-init-file()
+(defun open-init-file ()
   (interactive)
   (find-file "~/.emacs.d/init.el"))
 ;; Bind to <F2>
 (global-set-key (kbd "<f2>") 'open-init-file)
 
+(setq shell-num 0)
+(defun open-shell ()
+  (interactive)
+  (shell)
+  (rename-buffer (concat "shell" (number-to-string shell-num)))
+  (setq shell-num (+ shell-num 1)))
+(global-set-key (kbd "C-c s") 'open-shell)
 
-
-
+(defun resize-window ()
+  (message "underwork..."))
+;(global-set-key (kbd "C-c s") 'open-init-file)
 
 
 

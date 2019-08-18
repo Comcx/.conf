222c144ade3916a9bbb985eea9b8659ce7772ca7
Comcx <comcx@outlook.com>
2019-08-18 15:36:15 +0800

    add

--- UI/Text/Emacs/.emacs.d/init.el
+++ UI/Text/Emacs/.emacs.d/init.el
@@ -163,19 +163,3 @@
 
 
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "outline" :slant normal :weight normal :height 120 :width normal))))
- '(rainbow-delimiters-depth-1-face ((t (:foreground "gold"))))
- '(rainbow-delimiters-depth-2-face ((t (:foreground "deep sky blue"))))
- '(rainbow-delimiters-depth-3-face ((t (:foreground "orange"))))
- '(rainbow-delimiters-depth-4-face ((t (:foreground "coral"))))
- '(rainbow-delimiters-depth-5-face ((t (:foreground "green"))))
- '(rainbow-delimiters-depth-6-face ((t (:foreground "purple"))))
- '(rainbow-delimiters-depth-7-face ((t (:foreground "yellow"))))
- '(rainbow-delimiters-depth-8-face ((t (:foreground "cyan"))))
- '(rainbow-delimiters-depth-9-face ((t (:foreground "violet"))))
- '(rainbow-delimiters-unmatched-face ((t (:background "red")))))

97598529eecd60bfa1c90087a44cd958f6181d93
Comcx <comcx@outlook.com>
2019-10-04 12:27:12 +0800

    pretty

--- UI/Text/Emacs/.emacs.d/lisp/init-packages.el
+++ UI/Text/Emacs/.emacs.d/lisp/init-packages.el
@@ -59,6 +59,49 @@
 (global-set-key [f8] 'neotree-toggle)
 (setq neo-theme 'ascii)
 
+;; Math Symbols
+(require 'pretty-mode)
+(global-pretty-mode t)
+
+(pretty-deactivate-groups
+ '(:equality :ordering :ordering-double :ordering-triple
+             :arrows :arrows-twoheaded :punctuation
+             :logic :sets))
+
+(pretty-activate-groups
+ '(:sub-and-superscripts :greek :arithmetic-nary))
+
+(global-prettify-symbols-mode 1)
+
+(add-hook
+ 'python-mode-hook
+ (lambda ()
+   (mapc (lambda (pair) (push pair prettify-symbols-alist))
+         '(;; Syntax
+           ("def" .      #x2131)
+           ("not" .      #x2757)
+           ("in" .       #x2208)
+           ("not in" .   #x2209)
+           ("return" .   #x27fc)
+           ("yield" .    #x27fb)
+           ("for" .      #x2200)
+           ;; Base Types
+           ("int" .      #x2124)
+           ("float" .    #x211d)
+           ("str" .      #x1d54a)
+           ("True" .     #x1d54b)
+           ("False" .    #x1d53d)
+           ;; Mypy
+           ("Dict" .     #x1d507)
+           ("List" .     #x2112)
+           ("Tuple" .    #x2a02)
+           ("Set" .      #x2126)
+           ("Iterable" . #x1d50a)
+           ("Any" .      #x2754)
+           ("Union" .    #x22c3)))))
+
+
+
 
 
 (setq package-selected-packages my/packages)
